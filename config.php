f0c7a07df33482cb61768588aa2494bff9956eda
ricardofqueiroz <ricardoqueiroz81@hotmail.com>
2021-05-12 20:12:29 -0300

    Iniciamos o projeto

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 //Criando o arquivo de configuração
 spl_autoload_register(function($class_name)
 {
-	$filename = "class".DIRECTORY_SEPARATOR.$class_name. ".php";
+	$filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
 	if(file_exists($filename))
 {
 	require_once($filename);
