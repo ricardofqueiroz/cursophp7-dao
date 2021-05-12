67dfbc7866f5937f67eaed52e037a7a8b896ea42
ricardofqueiroz <ricardoqueiroz81@hotmail.com>
2021-05-12 00:06:32 -0300

    Criamos a classe usuario

--- class/Sql.php
+++ class/Sql.php
@@ -12,17 +12,17 @@ class Sql extends PDO
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", ""); //Caso tivesse senha colocaria root
 	}
     //Criando métodos
-	private function setParams($statment, $parameters = array())
+	private function setParams($statement, $parameters = array())
 	{
 		//Criar um foreach
 		foreach ($parameters as $key => $value)
 		{
-			$this->setParam($key, $value); 
+			$this->setParam($statement, $key, $value); 
 		}
 	}
 	private function setParam($statment, $key, $value)
 	{
-		$statment->bindParam($key, $value);
+		$statement->bindParam($key, $value);
 	}
 	//Fazer a execução dos comandos
 	public function query($rawQuery, $params = array())
--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 require_once("config.php");
 //Encontrar a classe Sql
 //Criar uma variável
+/*
 $sql = new sql();
 //Executar um comando do banco de dados
 //Criar uma variável pra receber nessa variável "$sql"
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //Fazer um echo json_encode da variável $usuarios
 echo json_encode($usuarios);
+*/
 
+//Chamar apenas um usuário registrado no banco de dados
+//Criar uma variável
+$root = new Usuario();
+//Chamar o método que foi criado na classe Usuario
+$root = loadById(3);
+echo $root;
 ?>
