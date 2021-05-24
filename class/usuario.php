a9da62b66e33b568f4519bdd7df6619b20c5cb35
ricardofqueiroz <ricardoqueiroz81@hotmail.com>
2021-05-24 20:53:51 -0300

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -7,7 +7,7 @@ class Usuario
 	private $deslogin;
 	private $dessenha;
 	private $dtcadastro;
-	//Criando a função com "get e set" de usuário
+	//Criando a função com "get e set" de idusuário
 	public function getIdusuario()
 	{
 		return $this->idusuario;
@@ -48,9 +48,9 @@ class Usuario
 	{
 		$sql = new sql();
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
-		//Obs: array são paraêmtros
+		//Obs: array são parâmetros
 
-		//Validar a variável resultas
+		//Validar a variável results
 		//if(isset($results[0]))
 		//Outra forma de fazer seria usando um count, qualquer uma das duas serve
 		if(count($results) > 0)
@@ -67,6 +67,48 @@ class Usuario
 		}
 	}
 	//Visualisar os dados vindo do banco
+
+	//Criar um método para listar todos usários que estão na tabela
+
+	public static function getlist() //Obs. Quando é estatico não precisa instanciar o objeto
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	//Criar um método pra buscar um usuário no banco
+
+	public static function search($login) //Obs. Também é estatico, não ira precisar instanciar
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"));
+	}
+
+	//Criar um método pra obter os dados do usuário autenticado
+
+	public function login($login, $password) //Obs. Neste caso não foi usado estatico porque esta sendo utilizado os "gets e sets".
+	{
+		$sql = new sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN and dessenha = :PASSWORD", array(
+			":LOGIN"=>$login, ":PASSWORD"=>$password));
+		
+		if(count($results) > 0)
+		{
+            //Obs: Como é um array sempre como a posição "0".
+			$row = $results[0];
+            //Pegar os dados e mandar para o sets
+   			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+	}
+	else
+	{
+		throw new Exception("Login e/ou senha inválidos");
+		
+	}
+}
+
 	//Criar uma função com o método mágiso __toString
 	public function __toString()
 	{
--- index.php
+++ index.php
@@ -13,9 +13,23 @@ echo json_encode($usuarios);
 */
 
 //Chamar apenas um usuário registrado no banco de dados
+//Carrega um usuário
 //Criar uma variável
-$root = new Usuario();
+//$root = new Usuario();
 //Chamar o método que foi criado na classe Usuario
-$root->loadbyId(3);
-echo $root;
-?>
+//$root->loadbyId(3);
+//echo $root;
+
+//Carrega uma lista de usuário
+//$lista = Usuario::getlist(); //Obs. ::getlista porque é estatico. Quando é estatico não precisa instanciar o objeto
+//echo json_encode($lista);
+
+//Carregar uma lista de usuáriois buscando pelo login
+//$search = Usuario::search("jo"); //No caso ira retornar todos os usuários que tem login com a "jo".
+//echo json_encode($search);
+
+//Carregar um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$"); //Caso login e senha não existe será exibido "login e ou senha inválidos". 
+echo $usuario;
+?>  
